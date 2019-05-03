be2fa62eec8ae71a53f207b89f72eb564fc319a7
sayed.musavy@integrify.io <sayed.musavy@integrify.io>
2019-05-03 10:05:49 +0300

    checkbox examples

--- src/App.js
+++ src/App.js
@@ -5,23 +5,37 @@ class App extends Component {
     super()
     this.state = {
       firstName: "",
-      lastName: ""
+      lastName: "",
+      isFriendly:true
     }
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
+    const {name,value}=event.target
     this.setState({
-      [event.target.name]: event.target.value
+      [name]:value
     })
   }
 
+ 
   render() {
     return (
       <form>
         <input type="text" value={this.state.firstName} name="firstName" placeholder="First Name" onChange={this.handleChange} />
         <br />
         <input type="text" value={this.state.lastName} name="lastName" placeholder="Last Name" onChange={this.handleChange} />
+
+
+        <textarea value={"some Default value"} />
+<br/>
+<lable>
+<input type="checkbox"
+name="isFriendly"
+checked={this.state.isFriendly}
+onChange={this.handleChange} />
+is Friendly
+        </lable>
         <h1>{this.state.firstName} {this.state.lastName}</h1>
       </form>
     )
